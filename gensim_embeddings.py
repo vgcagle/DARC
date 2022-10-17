ce23ebfb7d27cbcd00c17e73181b96bae7e8873f
rpersing <rpersing@mail.umw.edu>
2022-10-17 14:56:51 -0400

    rewrote NER in spaCy and created word embeddings

---
+++ gensim_embeddings.py
@@ -0,0 +1,45 @@
+from distutils.command.clean import clean
+from gensim.models import Word2Vec
+from gensim.models.keyedvectors import KeyedVectors
+from sklearn.decomposition import PCA
+from matplotlib import pyplot as plt
+import numpy as np
+import string
+
+def get_word_embeds():
+    punctuation_stripper = str.maketrans('', '', string.punctuation)
+
+    f = open("spacy_masked_sents.txt", "r")
+
+    sentences = f.readlines()
+
+    f.close()
+
+    cleaned_sents_w_labels = [sent.strip().split(",") for sent in sentences] # cleaned sentences with labels
+    # print(cleaned_sents_w_labels)
+    cleaned_sents_wo_labels = [sent.translate(punctuation_stripper).replace("True", "").replace("False", "").split() for sent in sentences] # cleaned labels with labels
+    # print(cleaned_sents_wo_labels)
+    model = Word2Vec(sentences=cleaned_sents_wo_labels, min_count=1, vector_size=30) # word2vec model
+    vectors = model.wv.vectors # all word vectors in model
+
+    X = vectors
+
+    # sklearn PCA decomposition for word vectors
+    pca = PCA(n_components=2)
+    result = pca.fit_transform(X)
+
+
+    # creating scatter plot for vectors
+    plt.scatter(result[:, 0], result[:, 1])
+
+    words = list(model.wv.index_to_key)
+
+    # print(len(words))
+
+    for ndx, word in enumerate(words):
+        plt.annotate(word, xy=(result[ndx, 0], result[ndx, 1]))
+
+    vx, vy = [vector for vector in pca.components_], [sent[-1] for ndx, sent in enumerate(cleaned_sents_w_labels)]
+
+    return vx, vy
+
---
+++ spacyNER.py
@@ -0,0 +1,63 @@
+import os
+import spacy
+from spacy.vectors import Vectors
+from spacy.vocab import Vocab
+# import nltk
+# from nltk.stem.snowball import SnowballStemmer
+
+nlp = spacy.load("en_core_web_lg")
+# sprint(nlp.pipe_names)
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) # get path to directory containing synthetic data
+dir_path = os.path.join(dir_path, "synthetic") # join path to data directory
+directory = os.path.join(dir_path, "data")
+sms_file = open("spacy_masked_sents.txt", "w")
+
+for dir in os.listdir(directory): # loop through data directory
+    subdir = os.path.join(directory, dir) # get sub-directory
+    for filename in os.listdir(subdir): # loop through files in current sub-directory
+        f = os.path.join(subdir, filename) # get filename paths
+        if os.path.isfile(f): # check if path is a file
+            with open(f) as txt_file: # open file
+                lines = txt_file.readlines() # read the lines of a file
+
+                for line in lines:
+                    
+                    doc = nlp(line.strip())
+
+                    org_list = []
+                    org_count = 0
+
+                    for ent in doc.ents:
+                        if ent.label_ == "ORG":
+                            org_count +=1
+                            org_list.append(ent.text)
+                        
+                    # sentence masking
+                    if org_count == 2:
+                        # masked_sent = line.replace(f"{org_list[0]}'s", "NEFROM")
+                        masked_sent = line.replace(f"{org_list[0]}", "NEFROM")
+                        masked_sent = masked_sent.replace(org_list[1], "NETOOO")
+                    else:
+                        continue
+
+                    
+                    # stemmer = SnowballStemmer(language='english')
+                    # masked_sent = " ".join([stemmer.stem(word) for word in masked_sent.split()])
+                    # print(masked_sent)
+
+                    # processing the masked sentence
+                    new_doc = nlp(masked_sent.strip())
+
+                    # print([(key, new_doc.vocab.strings[key]) for key in new_doc.vocab.vectors.keys()])
+
+                    # removing stop words and getting word lemmas
+                    final_masked_sent = " ".join([token.lemma_ for token in new_doc if not token.is_punct and not token.is_stop])
+                    # final_masked_sent = masked_sent.replace("nefrom", "NEFROM")
+                    # final_masked_sent = final_masked_sent.replace("netooo", "NETOOO")
+                    print(final_masked_sent)
+
+                    if dir == "A_sup_B":
+                        sms_file.write(f"{final_masked_sent},True\n")
+                    else:
+                        sms_file.write(f"{final_masked_sent},False\n")
