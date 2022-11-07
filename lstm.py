9cf8d032a92836fca9ae827b80cf41511ef26504
Veronica <vgcagle@gmail.com>
2022-11-07 12:00:34 -0500

    Add metrics

--- lstm.py
+++ lstm.py
@@ -41,28 +41,23 @@ from sklearn.preprocessing import LabelBinarizer
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras.layers import LSTM
+from keras.layers import LSTM, Bidirectional
 from keras.utils import np_utils
 from keras.optimizers import Adam
 
 from keras.layers import Flatten
 from keras.layers import Embedding
 
+import tensorflow as tf
+
 
 np.set_printoptions(suppress=True)
-
-
-
-
-
 
 X,y = get_word_embeds()
 
 #Make the False/True labels 0/1 
 lb = LabelBinarizer(neg_label=0, pos_label=1)
 y = lb.fit_transform(y)
-
-import tensorflow as tf
 
 #Reshape X to correct size for lstm 
 X = np.reshape(X, X.shape + (1,))
@@ -74,9 +69,8 @@ classifier = Sequential()
 #Adding the input LSTM network layer
 classifier.add(LSTM(128, input_shape=(Xtrain.shape[1:]), return_sequences=True))
 classifier.add(Dropout(0.5))
-#vocab_size= 10
-#max_length = 2
-#classifier.add(Embedding(vocab_size, 32, input_length=max_length))
+
+#classifier.add(Embedding(100, 32, input_length=2))
 #Adding a second LSTM network layer
 #classifier.add(LSTM(128))
 
@@ -94,29 +88,30 @@ classifier.compile( loss='binary_crossentropy',
 #Fitting the data to the model
 classifier.fit(Xtrain,
          ytrain,
-          epochs=12,
-          validation_data=(Xtest, ytest))
+          epochs=22,
+          batch_size= 32)
+          #validation_data=(Xtest, ytest))
 
 
 test_loss, test_acc = classifier.evaluate(Xtest, ytest)
 print('Test Loss: {}'.format(test_loss))
 print('Test Accuracy: {}'.format(test_acc))
 
-#ypred = classifier.predict(Xtest)
+#Make predictions
+y_pred1 = classifier.predict(Xtest)
+y_pred = np.argmax(y_pred1, axis=1)
 
-# for ndx, pred in enumerate(ypred):
-#     if pred[0] < 0.5:
-#         ypred[ndx] = int(0)
-#     else:
-#         ypred[ndx] = int(1)
-#predicted_cat = 
-#need to make these values 0 and 1 
-# print(confusion_matrix(ytest,ypred))
-# print(f1_score(ytest, ypred))
-# print(precision_score(ytest, ypred))
-# print(recall_score(ytest, ypred))
+# Print f1, precision, and recall scores
+print(precision_score(ytest, y_pred , average="macro"))
+print(recall_score(ytest, y_pred , average="macro"))
+print(f1_score(ytest, y_pred , average="macro"))
 
-# vocab_size= 2000
-# max_length = 2
-# classifier.add(Embedding(vocab_size, 8, input_length=max_length))
-
+ypred1 = classifier.predict(Xtest)
+ypred = np.argmax(ypred1, axis=1)
+print(f1_score(ytest, ypred, average = 'micro'))
+print(precision_score(ytest, ypred, average = 'micro'))
+print(recall_score(ytest, ypred, average = 'micro'))
+print("")
+print(f1_score(ytest, ypred))
+print(precision_score(ytest, ypred))
+print(recall_score(ytest, ypred))
