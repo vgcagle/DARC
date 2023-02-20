9fa04be2887d932b2c4ccfa2cd9ac78eab4272eb
Stephen <stephen@umw.edu>
2023-02-20 15:17:34 -0500

    Play around with spaCy's parser for BB.

---
+++ training/BreakingBad/bb.py
@@ -0,0 +1,19 @@
+
+import spacy
+import pandas as pd
+
+# Requires "python -m spacy download en_core_web_md"
+nlp = spacy.load("en_core_web_md")
+
+# So we can look at individual sentences.
+nlp.add_pipe("sentencizer")
+
+
+with open("s01ep001.txt", encoding="utf-8") as f:
+    doc = nlp(f.read())
+
+for sent in doc.sents:
+    stuff = [ 'text','pos_','lemma_','tag_','dep_', 'ent_type_', 'ent_iob_' ]
+    bb = pd.DataFrame({ s: [ getattr(t,s) for t in sent ] for s in stuff })
+    print(bb)
+    input("Press ENTER.")
